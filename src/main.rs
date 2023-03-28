cb13216d4ad708bb6211400d1a238c9f396285df
tippfehlr <tippfehlr@gmail.com>
2023-03-28 18:23:17 +0200

    working millis(), interface model

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ use arduino_hal::{hal::Atmega, usart::UsartOps, Usart};
 use core::cell;
 use panic_halt as _;
 
-const PRESCALER: u32 = 1024;
-const TIMER_COUNTS: u32 = 125;
+const PRESCALER: u32 = 64;
+const TIMER_COUNTS: u32 = 250;
 const MILLIS_INCREMENT: u32 = PRESCALER * TIMER_COUNTS / 16000;
 
 static MILLIS_COUNTER: avr_device::interrupt::Mutex<cell::Cell<u32>> =
@@ -23,16 +23,16 @@ static MILLIS_COUNTER: avr_device::interrupt::Mutex<cell::Cell<u32>> =
 fn millis_init(tc0: arduino_hal::pac::TC0) {
     // Configure the timer for the above interval (in CTC mode)
     // and enable its interrupt.
-    tc0.tccr0a.write(|w| w.wgm0().ctc());
-    tc0.ocr0a.write(|w| w.bits(TIMER_COUNTS as u8));
+    tc0.tccr0a.write(|w| w.wgm0().ctc()); // timer control register 0a
+    tc0.ocr0a.write(|w| w.bits(TIMER_COUNTS as u8)); // output compare register 0a
     tc0.tccr0b.write(|w| match PRESCALER {
         8 => w.cs0().prescale_8(),
         64 => w.cs0().prescale_64(),
         256 => w.cs0().prescale_256(),
         1024 => w.cs0().prescale_1024(),
         _ => panic!(),
-    });
-    tc0.timsk0.write(|w| w.ocie0a().set_bit());
+    }); // timer control register 0b
+    tc0.timsk0.write(|w| w.ocie0a().set_bit()); // timer interrupt mask register 0
 
     // Reset the global millisecond counter
     avr_device::interrupt::free(|cs| {
@@ -58,19 +58,62 @@ fn millis() -> u32 {
 
 enum MenuState {
     Main,
+    /// wall lamps
+    /// id’s 0-3
     Lamp1,
+    /// desk lamps
+    /// id’s 4-6
     Lamp2,
-    Lamp3,
-    Lamp4,
-    Lamp5,
-    Lamp6,
-    Lamp7,
-    Lamp8,
+    // /// power outlets
+    // /// id’s 4-5
+    // Lamp3,
+    // /// strahler
+    // /// id 9
+    // Lamp4,
+    // /// Bastelecke
+    // /// id 10
+    // Lamp5,
+    // ///
+    // Lamp6,
+    // Lamp7,
+    // Lamp8,
+    // …
 }
 
-struct Menu {
-    mode: MenuState,
-    last_update: u32,
+enum Button {
+    None,
+
+    /// increase blue
+    SlideUp,
+
+    /// decrease blue
+    SlideDown,
+
+    /// move focus point left
+    SlideLeft,
+
+    /// move focus point right
+    SlideRight,
+
+    /// if selected turn wall lamps on/off
+    /// else select wall lamps
+    PressTop,
+
+    /// if selected turn desk lamps on/off
+    /// else select desk lamps
+    PressBottom,
+
+    /// select next lamp
+    PressRight,
+
+    /// select previous lamp
+    PressLeft,
+
+    /// increase brightness
+    RotateRight,
+
+    /// decrease brightness
+    RotateLeft,
 }
 
 #[arduino_hal::entry]
@@ -82,6 +125,7 @@ fn main() -> ! {
 
     millis_init(dp.TC0);
 
+    unsafe { avr_device::interrupt::enable() };
     // replace pins with correct ones
     let dir_buttons = pins.a0.into_analog_input(&mut adc);
     let poti_left_x = pins.a1.into_analog_input(&mut adc);
@@ -89,43 +133,23 @@ fn main() -> ! {
     let poti_right_x = pins.a3.into_analog_input(&mut adc);
     let poti_right_y = pins.a4.into_analog_input(&mut adc);
 
-    let mut menu = Menu {
-        mode: MenuState::Main,
-        last_update: 0,
-    };
+    let mut button: Button = Button::None;
+    let mut menu_state: MenuState = MenuState::Main;
 
     loop {
+        arduino_hal::delay_ms(1000);
+        // ufmt::uwriteln!(&mut serial, "{}", millis()).unwrap();
+
         match dir_buttons.analog_read(&mut adc) {
             // replace with values, these are switch positions
-            // Lamp1: Tischlampen
-            1 => match menu.mode {
-                MenuState::Main => {
-                    menu.mode = MenuState::Lamp1;
-                    menu.last_millis = millis();
-                }
-                MenuState::Lamp1 => {
-                    unimplemented!(); // toggle lamp1
-                }
-                _ => {}
-            },
-            2 => match menu.mode {
-                Button::Main => {
-                    menu.mode = Menu::Lamp2;
-                    menu.last_millis = millis();
-                }
-                Button::Lamp2 => {
-                    unimplemented!(); // toggle Lamp2
-                }
-            },
-            3 => {}
-            4 => {}
-            5 => {}
-            6 => {}
-            7 => {}
-            8 => {}
+            1 => button = Button::PressTop,
+            2 => button = Button::PressLeft,
+            3 => button = Button::PressRight,
+            4 => button = Button::PressBottom,
             _ => {}
         }
 
+        // sliding overwrites pressing
         match (
             poti_left_x.analog_read(&mut adc),
             poti_left_y.analog_read(&mut adc),
@@ -134,22 +158,39 @@ fn main() -> ! {
         ) {
             // replace with values, these are switch positions
             // left up, right down (rotating to right)
-            (7, 7, 7, 7) => {
-                send_data(&mut serial, 0b0000_0000_0000_0000, 0, 0, 0);
-            }
+            (7, 7, 7, 7) => button = Button::RotateRight,
+
             // left down, right up (rotating to left)
-            (4, 1, 1, 1) => {}
+            (4, 1, 1, 1) => button = Button::RotateLeft,
+
             // both up
-            (1...2, 1...2, 1...2, 1...2) => {}
+            (1...2, 1...2, 1...2, 1...2) => button = Button::SlideUp,
+
             // both down
-            (3, 3, 2, 2) => {}
-            (1, 4, 2, 2) => {}
+            (3, 3, 2, 2) => button = Button::SlideDown,
+
             // both left
-            (5, 5, 2, 2) => {}
+            (5, 5, 2, 2) => button = Button::SlideLeft,
+
             // both right
-            (6, 6, 2, 2) => {}
+            (6, 6, 2, 2) => button = Button::SlideRight,
+
             _ => {}
+        };
+
+        match button {
+            Button::RotateRight => send_data(&mut serial, get_mask(&menu_state), 10, 0, 0),
+            Button::RotateLeft => send_data(&mut serial, get_mask(&menu_state), -10, 0, 0),
+            _ => todo!(),
         }
+    }
+}
+
+fn get_mask(menu_state: &MenuState) -> u16 {
+    match menu_state {
+        MenuState::Main => 0b1111111111111111,
+        MenuState::Lamp1 => 0b0000000000001111,
+        MenuState::Lamp2 => 0b0000000001110000,
     }
 }
 
